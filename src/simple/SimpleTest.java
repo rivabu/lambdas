94accb8bf41cce4c7da33b0c183162e6a6329718
Rients <rientsvanburen@gmail.com>
2016-10-16 11:20:14 +0200

    first commit

---
+++ src/simple/SimpleTest.java
@@ -0,0 +1,10 @@
+package simple;
+
+public class SimpleTest {
+
+	public static void main(String[] args) {
+		System.out.println("test");
+
+	}
+
+}
---
+++ test/simple/SimpleTestTest.java
@@ -0,0 +1,16 @@
+package simple;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimpleTestTest {
+
+	SimpleTest testClass = new SimpleTest();
+	
+	@Test
+	public void test() {
+		testClass.main(null);
+	}
+
+}
